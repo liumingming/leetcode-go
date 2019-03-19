0beb5a8b9bef6f239c58611823da7fcd0441d92f
liumingming <liumingming@qutoutiao.net>
2019-03-19 13:09:39 +0800

    add binaryTree and preorder,midorder,postorder

--- basic/binary_tree.go
+++ basic/binary_tree.go
@@ -1,5 +1,132 @@
 package basic
 
+import "fmt"
 
 /*二叉树*/
+type BinaryTree struct {
+	root *TreeNode
+}
+
+type TreeNode struct {
+	left *TreeNode
+	right *TreeNode
+	data int
+}
+
+func NewBinaryTree() *BinaryTree {
+	return &BinaryTree{}
+}
+
+func (b *BinaryTree) AddNode(data int)  {
+	node := &TreeNode{
+		data:data,
+	}
+	if b.root == nil {
+		b.root = node
+		return
+	}
+
+	if b.root.data > data {
+		if b.root.left == nil {
+			b.root.left = node
+			return
+		}
+		b.root.left.AddNode(node)
+	} else {
+		if b.root.right == nil {
+			b.root.right = node
+			return
+		}
+		b.root.right.AddNode(node)
+	}
+
+}
+
+func (t *TreeNode) AddNode(node* TreeNode)  {
+	if t.data > node.data {
+		if t.left == nil {
+			t.left = node
+			return
+		}
+		t.left.AddNode(node)
+	} else {
+		if t.right == nil {
+			t.right = node
+			return
+		}
+		t.right.AddNode(node)
+	}
+}
+
+/*前序遍历*/
+func (b *BinaryTree) PreOrder()  {
+	if b.root == nil {
+		return
+	}
+	fmt.Println(b.root.data)
+	b.root.left.PreOrder()
+	b.root.right.PreOrder()
+}
+
+func (t *TreeNode) PreOrder()  {
+	if t == nil {
+		return
+	}
+	fmt.Println(t.data)
+	if t.left != nil {
+		t.left.PreOrder()
+	}
+	if t.right != nil {
+		t.right.PreOrder()
+	}
+}
+
+/*后续遍历*/
+func (b *BinaryTree) PostOrder()  {
+	if b.root == nil {
+		return
+	}
+	b.root.left.PostOrder()
+	b.root.right.PostOrder()
+	fmt.Println(b.root.data)
+}
+
+func (t *TreeNode) PostOrder()  {
+	if t == nil {
+		return
+	}
+
+	if t.left != nil {
+		t.left.PostOrder()
+	}
+	if t.right != nil {
+		t.right.PostOrder()
+	}
+	fmt.Println(t.data)
+}
+
+/*中序遍历*/
+func (b *BinaryTree) MidOrder()  {
+	if b.root == nil {
+		return
+	}
+	b.root.left.MidOrder()
+	fmt.Println(b.root.data)
+	b.root.right.MidOrder()
+
+}
+
+func (t *TreeNode) MidOrder()  {
+	if t == nil {
+		return
+	}
+
+	if t.left != nil {
+		t.left.MidOrder()
+	}
+	if t.right != nil {
+		t.right.MidOrder()
+	}
+	fmt.Println(t.data)
+}
 
---
+++ basic/binary_tree_test.go
@@ -0,0 +1,40 @@
+package basic
+
+import "testing"
+
+func TestNewBinaryTree_MidOrder(t *testing.T) {
+	tree := NewBinaryTree()
+	tree.AddNode(3)
+	tree.AddNode(1)
+	tree.AddNode(2)
+	tree.AddNode(5)
+	tree.AddNode(4)
+	//
+	tree.MidOrder()
+}
+
+
+func TestNewBinaryTree_PostOrder(t *testing.T) {
+	tree := NewBinaryTree()
+	tree.AddNode(3)
+	tree.AddNode(1)
+	tree.AddNode(2)
+	tree.AddNode(5)
+	tree.AddNode(4)
+
+	tree.PostOrder()
+	//
+
+}
+
+func TestBinaryTree_PreOrder(t *testing.T) {
+	tree := NewBinaryTree()
+	tree.AddNode(3)
+	tree.AddNode(1)
+	tree.AddNode(2)
+	tree.AddNode(5)
+	tree.AddNode(4)
+	//
+	tree.PreOrder()
+}
+
